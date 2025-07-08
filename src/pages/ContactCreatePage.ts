9cbb58acc64d1d8c9bd0cd989326b9028fc04d73
CrDanil <zaborovschidanil@gmail.com>
2025-07-08 16:15:55 +0300

    itWorks

--- src/pages/ContactCreatePage.ts
+++ src/pages/ContactCreatePage.ts
@@ -5,6 +5,9 @@ import {UserData} from "../common/models/UserData";
 import {StringUtils} from "../common/utils/StringUtils";
 import {expect} from "@playwright/test";
 import {ContactData} from "../common/models/ContactData";
+import {pageFactory} from "./pageFactory";
+import {Pages} from "../enums/common.enum";
+import {ContactsList} from "./ContactsList";
 
 export class ContactCreatePage extends BasePage {
     constructor(page: Page) {
@@ -30,31 +33,57 @@ export class ContactCreatePage extends BasePage {
         await this.open("addContact");
     };
 
-
-    public async createContact(options?: {
+    public async createFakeContacts(options?: {
         skipFirstName?: boolean;
         skipLastName?: boolean;
-        testNum: number}): Promise<void> {
+        }): Promise<void> {
 
-        if (!options?.skipFirstName && options?.testNum == 0) {
+        const skipFirst = options?.skipFirstName ?? false;
+        const skipLast = options?.skipLastName ?? false;
+
+        if (!skipFirst) {
             await this.firstNameInputLocator.fill(StringUtils.randomizeUsername());
         }
-        if (!options?.skipLastName && options?.testNum == 0) {
+        if (!skipLast) {
             await this.lastNameInputLocator.fill(StringUtils.randomizeUsername());
-
-        } else if (options?.testNum == 1) {
-            await this.firstNameInputLocator.fill("Vlad");
-            await this.lastNameInputLocator.fill("svsese");
-            await this.emailInputLocator.fill("vdrvsefc@dsvdr.com");
-            await this.address1InputLocator.fill("dvdjhrvd");
-            await this.address2InputLocator.fill("lkjnvdlkjrnv");
-            await this.phoneInputLocator.fill("9998887766");
-            await this.cityInputLocator.fill("Los Angeles");
-            await this.SoPInputLocator.fill("California");
-            await this.PCInputLocator.fill("20220");
-            await this.countryInputLocator.fill("Cricova");
-            await this.DoBInputLocator.fill("2018-02-02");
         }
         await this.submitButtonLocator.click();
     }
+
+
+    public async createContacts(): Promise<void> {
+        await this.firstNameInputLocator.fill("Vlad");
+        await this.lastNameInputLocator.fill("svsese");
+        await this.emailInputLocator.fill("vdrvsefc@dsvdr.com");
+        await this.address1InputLocator.fill("dvdjhrvd");
+        await this.address2InputLocator.fill("lkjnvdlkjrnv");
+        await this.phoneInputLocator.fill("9998887766");
+        await this.cityInputLocator.fill("Los Angeles");
+        await this.SoPInputLocator.fill("California");
+        await this.PCInputLocator.fill("20220");
+        await this.countryInputLocator.fill("Cricova");
+        await this.DoBInputLocator.fill("2018-02-02");
+
+        let contact = new ContactData();
+        contact.setContact({
+            firstname: "Vlad",
+            lastname: "svsese",
+            email: "vdrvsefc@dsvdr.com",
+        } as ContactData);
+
+        await this.submitButtonLocator.click();
+
+        const pageObj = pageFactory(this.page, Pages.ADD_CONTACT);
+        await pageObj.navigate();
+        await this.cancelButtonLocator.isVisible()
+
+        await this.firstNameInputLocator.fill("Svse");
+        await this.lastNameInputLocator.fill("kjvnsd");
+        await this.emailInputLocator.fill("vdrvsefc@dsvdr.com");
+        await this.address1InputLocator.fill("dvdjhrvd");
+        await this.address2InputLocator.fill("lkjnvdlkjrnv");
+        await this.SoPInputLocator.fill("California");
+
+        await this.submitButtonLocator.click();
+        }
 }
